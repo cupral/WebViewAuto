c2ded40829b74a746e69abead96e1c1b906c5a11
nerone-github <nerone-github@users.noreply.github.com>
2018-01-09 19:14:07 +0100

    new UI

---
+++ mobile/src/main/java/org/openauto/webviewauto/HTMLInterfaceMenu.java
@@ -0,0 +1,29 @@
+package org.openauto.webviewauto;
+
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+
+public class HTMLInterfaceMenu {
+
+    private Context context;
+
+    HTMLInterfaceMenu(Context context) {
+        this.context = context;
+    }
+
+    @JavascriptInterface
+    public void openMenu(String str) {
+        if (context instanceof WebViewAutoActivity){
+            ((WebViewAutoActivity)context).openDrawer();
+        }
+    }
+
+    @JavascriptInterface
+    public void toggleKeyboard(String str) {
+        if (context instanceof WebViewAutoActivity){
+            ((WebViewAutoActivity)context).toggleKeyboard();
+        }
+    }
+
+
+}

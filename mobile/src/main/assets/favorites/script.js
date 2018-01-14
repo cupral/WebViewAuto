dfa1524eaee8b59d1b500e6e384b336bc479e313
nerone-github <nerone-github@users.noreply.github.com>
2018-01-14 14:01:20 +0100

    new favorite overview screen

--- mobile/src/main/assets/favorites/script.js
+++ mobile/src/main/assets/favorites/script.js
@@ -10,13 +10,22 @@ function resetFavorites(){
     Android.resetFavorites("");
 }
 
+function getFaviconUrl(url){
+    var lastChar = url.slice(-1);
+    if(lastChar === "/"){
+        return url + "favicon.ico";
+    } else {
+        return url + "/favicon.ico";
+    }
+}
+
 function parseFavorites(favoritesJson){
     var favObjects = JSON.parse(favoritesJson);
     $(".favorite-container").empty();
     $.each(favObjects, function(i,e){
         var line1 = "<div class='favorite-item' data-url='"+e.url+"' data-desktop='false' onclick='openFavorite(this);'>";
         var line2 = "<div>"+e.title+"</div>";
-        var line3 = "<img class='favorite-img' src='"+e.url+"/favicon.ico'/>";
+        var line3 = "<img class='favorite-img' src='"+getFaviconUrl(e.url)+"'/>";
         var line4 = "</div>";
         var html = line1+line2+line3+line4;
         $(".favorite-container").append(html);

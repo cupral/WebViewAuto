9365f161a3719267bb30499257b10f1ac0c1d1eb
Luca Jannace <ljannace@gmail.com>
2018-02-27 22:27:24 +0100

    Remove unused stuff

--- mobile/src/main/java/org/openauto/webviewauto/utils/IconUtil.java
+++ mobile/src/main/java/org/openauto/webviewauto/utils/IconUtil.java
@@ -2,7 +2,6 @@ package org.openauto.webviewauto.utils;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
 import android.util.Base64;
 import android.util.Log;
 
@@ -32,26 +31,6 @@ public class IconUtil {
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
         byte[] array = stream.toByteArray();
         return "data:image/png;base64," + Base64.encodeToString(array, 0);
-    }
-
-    /**
-     * from : https://stackoverflow.com/questions/4837715/how-to-resize-a-bitmap-in-android
-     */
-    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
-        int width = bm.getWidth();
-        int height = bm.getHeight();
-        float scaleWidth = ((float) newWidth) / width;
-        float scaleHeight = ((float) newHeight) / height;
-        // CREATE A MATRIX FOR THE MANIPULATION
-        Matrix matrix = new Matrix();
-        // RESIZE THE BIT MAP
-        matrix.postScale(scaleWidth, scaleHeight);
-
-        // "RECREATE" THE NEW BITMAP
-        Bitmap resizedBitmap = Bitmap.createBitmap(
-                bm, 0, 0, width, height, matrix, false);
-        bm.recycle();
-        return resizedBitmap;
     }
 
     public static Bitmap getBitmapFromURL(String src) {
